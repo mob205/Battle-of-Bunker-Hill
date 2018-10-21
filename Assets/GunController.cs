ba429a0cd0933e700e38a021f0ea6b48c9ad946b
Donny M <Mobslayer205@gmail.com>
2018-10-20 21:16:10 -0400

    Gun aims at cursor.

--- Assets/GunController.cs
+++ Assets/GunController.cs
@@ -8,10 +8,16 @@ public class GunController : MonoBehaviour {
     [SerializeField]Image crosshair;
 
     CanvasScaler canvasScaler;
+    Camera mainCamera;
+    float resolutionX;
+    float resolutionY;
 	// Use this for initialization
 	void Start () {
         canvasScaler = crosshair.GetComponentInParent<CanvasScaler>();
-        Cursor.visible = false;
+        //Cursor.visible = false;
+        resolutionX = canvasScaler.referenceResolution.x;
+        resolutionY = canvasScaler.referenceResolution.y;
+        mainCamera = GetComponentInParent<Camera>();
 	}
 	
 	// Update is called once per frame
@@ -22,13 +28,18 @@ public class GunController : MonoBehaviour {
 
     void MoveCrosshair()
     {
-        Debug.Log(Input.mousePosition.x + ", " + Input.mousePosition.y);
-        //crosshair.rectTransform.anchoredPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        crosshair.rectTransform.anchoredPosition = new Vector2(Input.mousePosition.x * canvasScaler.referenceResolution.x / Screen.width,
-            Input.mousePosition.y * canvasScaler.referenceResolution.y / Screen.height);
+        crosshair.rectTransform.anchoredPosition = new Vector2(Input.mousePosition.x * resolutionX / Screen.width,
+            Input.mousePosition.y * resolutionY / Screen.height);
     }
-    void AimGun()
-    {
-        transform.LookAt(Input.mousePosition);
+    void AimGun() {
+        //var aimLocation = new Vector3(2 * ((Input.mousePosition.x - (resolutionX / 2)) / resolutionX),
+        //    2 * ((Input.mousePosition.y - (resolutionX / 2)) / canvasScaler.referenceResolution.y), 0);
+        //Debug.Log(aimLocation);
+        //transform.LookAt(aimLocation);
+        //Debug.DrawRay(transform.position, aimLocation, Color.red, 0.1f);
+        
+        var aimLocation = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        transform.LookAt(aimLocation);
+        Debug.Log(aimLocation + ", " + Input.mousePosition);
     }
 }
